30fa6fdae32b73b7f92faeaf05411543208de5af
fgerards <frank.gerards@gerards-gbr.de>
2019-08-14 17:23:07 +0200

    softens php-version for ZendServer 2019

--- manifests/init.pp
+++ manifests/init.pp
@@ -96,7 +96,7 @@ class zendserver (
   $external_url          = undef,) inherits zendserver::params {
   validate_bool($manage_repos)
   validate_re($webserver, ['\Aapache|nginx\Z',], 'Only apache or nginx are supported.')
-  validate_re($phpversion, ['\A5.3|5.4|5.5|5.6|7.0|7.1|7.2\Z',], 'Only versions 5.4 to 7.2 are supported.')
+  validate_re($phpversion, ['\A5.3|5.4|5.5|5.6|7.0|7.1|7.2|2019.0.0|2019.0\Z',], 'Only versions 5.4 to 7.2 are supported.')
   validate_re($admin_api_key_name, ['^\w{5,20}$','api_key_name is required and must be 5-20 characters.'])
   validate_re($admin_api_key_secret, ['^\w{64}$','api_key_secret is required and must be 64 characters.'])
   anchor { 'zendserver::begin': } ->
